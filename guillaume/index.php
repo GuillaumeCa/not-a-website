155587b26d6265ddaf56cfdeadf8ea9a3f604f66
GuillaumeCa <guillaume.carre@isep.fr>
2016-02-15 23:55:19 +0100

    fix image

--- guillaume/index.php
+++ guillaume/index.php
@@ -12,10 +12,11 @@
 
     <?php
     if (isset($_GET['image'])) {
-      $image = $_GET['image'];
-    }
+      $image = $_GET['image'];?>
+      <img src="<?php echo $image ?>" alt="lol" width="300px" />
+    <?php}
     ?>
 
-    <img src="<?php echo $image ?>" alt="lol" width="300px" />
+
   </body>
 </html>
