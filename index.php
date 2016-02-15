3f7ddc2a748d7ee9432f4650038bfe21f9c4e465
ArnaudRib <arnaud.ribeyrolles@isep.fr>
2016-02-15 18:56:34 +0100

    :o

--- index.php
+++ index.php
@@ -5,6 +5,18 @@
     <title>Not a Website at all :x</title>
   </head>
   <body>
+    <header>
+
+    </header>
+
+    <div id="content">
+
+      <!--LETS WRITE BULLSHIT-->
+      
+    </div>
+
+    <footer>
+    </footer>
 
   </body>
 </html>
