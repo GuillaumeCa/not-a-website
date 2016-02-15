4d1e1ee5d5c4d00700d4bd4b24fdc89ad0a4312e
GuillaumeCa <guillaume.carre@isep.fr>
2016-02-15 20:06:11 +0100

    fix position

--- index.php
+++ index.php
@@ -8,15 +8,8 @@
   </head>
   <body>
     <video src="lien de la vidéo :P" autoplay class="bg-vid">
-
-<<<<<<< HEAD
     </video>
     <h1>not a website :x</h1>
-=======
-    <header>
-
-    </header>
->>>>>>> bc79a016a0a2fdb91ab80b55cce2b72b237440e3
 
     <div class="content">
       <a href="arnaud" style="float:left;">Page de ouf :o</a>
